be6121431f361a40bc5a53748cf81f4d05d440d8
Icaro <icaropessoalobo@gmail.com>
2024-11-07 12:32:37 -0300

    Initial code

---
+++ script.js
@@ -0,0 +1,73 @@
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() { // generate random number between 0-2
+    return Math.floor(Math.random() * 3);
+}
+
+function getHumanChoice() { // ask user for input
+    return prompt('Type your choice').toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === 'rock') && (computerChoice === 1)) {
+        console.log('You lose! Paper beats Rock');
+        computerScore++;
+    }
+    else if ((humanChoice === 'rock') && (computerChoice === 2)) {
+        console.log('You win! Rock beats Scissors');
+        humanScore++;
+    }
+    else if ((humanChoice === 'paper') && (computerChoice === 0)) {
+        console.log('You win! Paper beats Rock');
+        humanScore++;
+    }
+    else if ((humanChoice === 'paper') && (computerChoice === 2)) {
+        console.log('You lose! Scissors beats Paper');
+        computerScore++;
+    }
+    else if ((humanChoice === 'scissors') && (computerChoice === 0)) {
+        console.log('You lose! Rock beats Scissors');
+        computerScore++;
+    }
+    else if ((humanChoice === 'scissors') && (computerChoice === 1)) {
+        console.log('You win! Scissors beats Paper');
+        humanScore++;
+    }
+    else {
+        console.log('It\'s a tie!');
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice()); 
+    }
+    humanScore > computerScore ? console.log(`Your score: ${humanScore}, Computer score: ${computerScore}. You win!`)
+        : humanScore < computerScore ? console.log(`Your score: ${humanScore}, Computer score: ${computerScore}. You lose!`)
+        : console.log(`Your score: ${humanScore}, Computer score: ${computerScore}. It's a tie!`);
+}
+
+playGame();
+
+
+
+
+// assign each number to either rock, paper or scissors
+
+// compare if user input beat computer choice
+
+
+const output = document.querySelector('.output');
+output.textContent = "";
+
+// let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
+
+
